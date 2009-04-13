494d3baf93fba0fd552e3d6287ccf885740ed2f2
Mark Kimsal <google@fossradio.com>
2009-04-13 12:08:16 -0400

    show execution time at top

--- index.php
+++ index.php
@@ -25,10 +25,12 @@
  */
 
 $start = microtime(1);
+/*
 if (version_compare(phpversion(), '5.2.0', '<')===true) {
     echo  '<div style="font:12px/1.35em arial, helvetica, sans-serif;"><div style="margin:0 0 25px 0; border-bottom:1px solid #ccc;"><h3 style="margin:0; font-size:1.7em; font-weight:normal; text-transform:none; text-align:left; color:#2f2f2f;">Whoops, it looks like you have an invalid PHP version.</h3></div><p>Magento supports PHP 5.2.0 or newer. <a href="http://www.magentocommerce.com/install" target="">Find out</a> how to install</a> Magento using PHP-CGI as a work-around.</p></div>';
     exit;
 }
+ */
 
 $AOFilename = 'app/AO.php';
 
@@ -45,14 +47,41 @@ require_once $AOFilename;
 
 #Varien_Profiler::enable();
 
-#Mage::setIsDeveloperMode(true);
+AO::setIsDeveloperMode(true);
 
 #ini_set('display_errors', 1);
 
+
+/*
+register_tick_function('tick_profiler');
+declare(ticks=2);
+ */
+
+
 umask(0);
 AO::run();
+
+
+if (AO::getIsDeveloperMode(true)) {
 $end = microtime(1);
 if (! strstr($_SERVER['PHP_SELF'], 'ajax')) {
-	echo sprintf('%.2f',($end - $start)*1000);
-	echo ' - '.memory_get_peak_usage();
+	echo '<div style="position:absolute;top:0px;background-color:#cfc">';
+	echo sprintf('%.2f',($end - $start)*1000). 'ms';
+	echo ' - '. number_format(memory_get_peak_usage()/1024).' kb';
+	echo '</div>';
 }
+}
+
+
+
+/*
+function tick_profiler($return=false) {
+	   static $m=0;
+//	   if ($return) return "$m bytes";
+	   if (($mem=memory_get_usage())>$m) $m = $mem;
+//	   if ($mem >1850000) {echo $mem ." "; bt_die();}
+}
+
+
+echo tick_profiler(true);
+ */
